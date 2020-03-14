37c3bff572dcc48949f0c7febb7f546bbec23f2c
fawda123 <mbafs2012@gmail.com>
2020-03-14 13:03:35 -0400

    initial figures added

---
+++ R/funcs.R
@@ -0,0 +1,6 @@
+# get legend from an existing ggplot object
+g_legend<-function(a.gplot){
+  tmp <- ggplot_gtable(ggplot_build(a.gplot))
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  return(legend)}
